b3c2e502323c6ed9025aae36473aaac1a37eb039
Jorj X. McKie <jorj.x.mckie@outlook.de>
2019-07-10 08:14:57 -0400

    initializing

---
+++ test-geo.py
@@ -0,0 +1,12 @@
+import fitz
+import sys
+
+print("Python:", sys.version)
+print(fitz.__doc__)
+m1 = fitz.Matrix(101)
+m2 = fitz.Matrix(-101)
+m3 = m1 * m2
+print()
+print("Checking matrix multiplication.")
+print("Rotations of same positive and negative angle should result in identity.")
+print("Result of abs(Identity - rot+ * rot-) =", abs(m3 - fitz.Identity))
