148b5f2c646b821f66acfecce0769023874fd79a
Jorj X. McKie <jorj.x.mckie@outlook.de>
2019-07-11 05:52:45 -0400

    more checks

--- test-geo.py
+++ test-geo.py
@@ -1,16 +1,25 @@
 from __future__ import print_function
 import fitz
 import sys
+import time
 
 bitness = "64 bit" if sys.maxsize > 2 ** 32 else "32 bit"
 
 print("Python:", sys.version, bitness, "on", sys.platform, "\n")
 print(fitz.__doc__)
 
-m1 = fitz.Matrix(101)
-m2 = fitz.Matrix(-101)
+alpha = 255
+m1 = fitz.Matrix(alpha)
+m2 = fitz.Matrix(-alpha)
 m3 = m1 * m2
 print()
-print("Checking matrix multiplication.")
-print("Rotations of same positive and negative angle should result in identity.")
-print("Result of abs(Identity - rot+ * rot-) =", abs(m3 - fitz.Identity))
+print("Checking 'Matrix(%i) * Matrix(-%i) == Identity'." % (alpha, alpha))
+print("Deviation: %g" % abs(m3 - fitz.Identity))
+
+repeats = 10000
+t0 = time.perf_counter()
+for i in range(repeats):
+    m3 = m1 * m2
+t1 = time.perf_counter()
+quot = (t1 - t0) / repeats
+print("Average multiplication time: %g sec" % quot)
