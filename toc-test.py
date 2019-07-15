83a9fd048ce69efb5497e2e413af006fef93a46f
Jorj X. McKie <jorj.x.mckie@outlook.de>
2019-07-15 18:50:13 -0400

    add new test commands

---
+++ toc-test.py
@@ -0,0 +1,23 @@
+#! python
+from __future__ import print_function
+import fitz
+import sys
+
+print("")
+print(__file__)
+print("".ljust(30, "-"))
+print("Python:", sys.version)
+print(fitz.__doc__)
+
+datei = "001003ED.pdf"
+doc = fitz.open(datei)
+
+toc = doc.getToC(True)
+print("Simple ToC of file", datei, "".ljust(50, "="))
+for t in toc:
+    print(t)
+
+toc = doc.getToC(False)
+print("\n\n\nExtended ToC of file", datei, "".ljust(50, "="))
+for t in toc:
+    print(t)
